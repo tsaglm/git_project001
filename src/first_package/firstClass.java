55d23d4f4c45b6ae960af65e04d24cdcc5df7117
Tugba <tution2018@gmail.com>
2020-08-09 12:36:11 +0300

    Last commit from local master branch

--- src/first_package/firstClass.java
+++ src/first_package/firstClass.java
@@ -6,7 +6,9 @@ public class firstClass {
 		System.out.println("Our first commit from local repo");
 		System.out.println("Our second commit from local repository");
 		System.out.println("now I am on my Tugba branch");
-
+       System.out.println("I want to have conflict");
+       
+       System.out.println("I wann to have a second conflict");
 	}
 
 }
