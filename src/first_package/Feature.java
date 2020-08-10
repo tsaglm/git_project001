b94296e8a922df1c06669e38179e61c4e1a2157f
tsaglm <69387911+tsaglm@users.noreply.github.com>
2020-08-10 10:07:30 +0300

    Update Feature.java
    This is the third commit from remote master branch

--- src/first_package/Feature.java
+++ src/first_package/Feature.java
@@ -6,6 +6,6 @@ public class Feature {
 		System.out.println("This is my commit from feature branch");
 		System.out.println("This is the first commit from remote master branch");
 		System.out.println("This is the second commit from remote master branch");
-	}
+		System.out.println("This is the third commit from remote master branch");
 
 }
